73a8c5f2289c661b31a48f9f9ce7cd14fc1c42a1
Hakizimana Fred <hakifred20@gmail.com>
2023-08-26 12:56:32 +0300

    configured database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,26 @@
 import "dotenv/config";
 import express from "express";
 import { isValidPort } from "./utils";
+import { startDB } from "./config/db";
 
 const startServer = async (port: number) => {
   const app = express();
-  // DB
-  // Middlewares
-  // Routes
-
-  app.listen(port, () => {
-    console.log(`server started on port ${port}`);
-  });
+  try {
+    await startDB();
+    app.listen(port, () => {
+      console.log(`server started on port ${port}`);
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(`🍎 Error occurred when starting server: ${error.message}`);
+    } else {
+      console.log("Unexpected error", error);
+    }
+  }
 };
 
 const envPort: string | undefined = process.env.PORT;
 if (isValidPort(envPort)) {
   const parsedPort = parseInt(envPort as string, 10);
-  startServer(parsedPort);
+  startServer(parsedPort).catch((e) => console.log(e.message));
 }
